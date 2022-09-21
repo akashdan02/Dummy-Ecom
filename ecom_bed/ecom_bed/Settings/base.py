bc35a02e8172e019cd5ece3bdffad483e2042d2c
Akash <akashvignesh@appinessworld.com>
2022-09-21 16:17:27 +0530

    splitted  settings into base & development

--- ecom_bed/ecom_bed/Settings/base.py
+++ ecom_bed/ecom_bed/Settings/base.py
@@ -70,15 +70,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "ecom_bed.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
