b3d730e58dcc064d20b1e4054a9622b87a0ed949
Akash <akashvignesh@appinessworld.com>
2022-09-26 10:34:36 +0530

    added search filter

--- ecom_bed/app/catalog/admin.py
+++ ecom_bed/app/catalog/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import CatalogData,Brand,Category
 
-admin.site.register(Brand)
-admin.site.register(Category)
-admin.site.register(CatalogData)
+@admin.register(Brand)
+# admin.site.register(Category)
+# admin.site.register(CatalogData)
+
+class BrandAdmin(admin.ModelAdmin):
+    search_fields=('brand_name',)
+    list_display = ('brand_name','is_active','created_at','updated_at')
+    list_filter = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at')  
+
+@admin.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields=('id_brand__brand_name','category',)
+    list_display = ('id_brand','category','is_active','created_at','updated_at')
+    list_filter = ('id_brand','is_active',)
+    readonly_fields = ('created_at', 'updated_at')  
+
+@admin.register(CatalogData)
+
+class CatalogdataAdmin(admin.ModelAdmin):
+    search_fields=('title','id_brand__brand_name','id_category__category',)
+    list_display = ('id_brand','id_category','title','description','price','discountPercentage','rating','stock','thumbnail','images','is_active','created_at','updated_at')
+    list_filter = ('id_brand','is_active',)
+    readonly_fields = ('created_at', 'updated_at')  
